83910977d7356ae56c44f7dc445da3222da88a13
Mario-Mellek <mario.samy266@gmail.com>
2023-06-26 10:23:14 +0000

    A function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,11 +4,25 @@ void quickSort_recur(int arr[], int low, int high, size_t size);
 int partition(int arr[], int low, int high, size_t size);
 void swap(int *x, int *y, const int *arr, size_t size);
 
+/**
+ * quick_sort - wrapper function that helps
+ * sorts an array of integers in ascending order
+ * @array: The array to be sorted
+ * @size: size of arr
+*/
 void quick_sort(int *array, size_t size)
 {
 	quickSort_recur(array, 0, size - 1, size);
 }
 
+/**
+ * quickSort_recur - Recursive function that sorts
+ * a subarray (both sides) of integers in ascending order
+ * @arr: The subarray to be sorted
+ * @low: Subarray's lowest index
+ * @high: Subarray's highest index
+ * @size: size of arr
+*/
 void quickSort_recur(int arr[], int low, int high, size_t size)
 {
 	int pivotIdx;
@@ -21,6 +35,14 @@ void quickSort_recur(int arr[], int low, int high, size_t size)
 	}
 }
 
+/**
+ * partition - Partitions a subarray around a pivot value
+ * @arr: The subarray to be partitioned
+ * @low: Subarray's lowest index
+ * @high: Subarray's highest index
+ * @size: size of arr
+ * Return: the index of the pivot
+*/
 int partition(int arr[], int low, int high, size_t size)
 {
 	int pivotVal = arr[high];
@@ -41,6 +63,13 @@ int partition(int arr[], int low, int high, size_t size)
 	return (i);
 }
 
+/**
+ * swap - Swaps two elements in an array and prints the array after swapping
+ * @x: Pointer to first element to be swapped
+ * @y: Pointer to second element to be swapped
+ * @arr: Array to be swapped
+ * @size: size of arr
+*/
 void swap(int *x, int *y, const int *arr, size_t size)
 {
 	int temp = *x;
